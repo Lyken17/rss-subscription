05c5794b8f674194109a73afa76d5dff81729e3c
Ligeng Zhu <Lyken17@users.noreply.github.com>
2024-11-22 21:50:01 +0800

    update

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import json
 import xml.etree.ElementTree as ET
 import xmltodict
 import requests
+import shutil
 # Example usage
 if __name__ == "__main__":
     # xml_file_path = 'class.xml'
@@ -12,6 +13,8 @@ if __name__ == "__main__":
     # format class xml
     out = xmltodict.parse(open("class.xml", mode="r", encoding="utf-8").read())
     xmltodict.unparse(out, output=open("class.xml", mode="w", encoding="utf-8"), pretty=True)
+    # xmltodict.unparse(out, output=open("rss/mikan-full.xml", mode="w", encoding="utf-8"), pretty=True)
+    shutil.move("class.xml", "rss/mikan-full.xml")
     new_items = [] 
     items = out["rss"]["channel"]["item"]
     for item in items:
